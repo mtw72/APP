f38197ebad34d56ee4f2ee401e9ea4a1f5c5213c
mtw72 <mtwong72@gmail.com>
2024-03-08 17:51:12 +0000

    removed gmpx

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,18 +1,18 @@
-// Initialize and add the map
-function initMap() {
-    // The location of APP
-    const app = { lat: 22.276805936463386, lng: 114.16923333620271 };
-    // The map, centered at APP
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 18,
-        center: app,
-    });
-    // The marker, positioned at APP
-    const marker = new google.maps.Marker({
-        position: app,
-        map: map,
-    });
-}
+// // Initialize and add the map
+// function initMap() {
+//     // The location of APP
+//     const app = { lat: 22.276805936463386, lng: 114.16923333620271 };
+//     // The map, centered at APP
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 18,
+//         center: app,
+//     });
+//     // The marker, positioned at APP
+//     const marker = new google.maps.Marker({
+//         position: app,
+//         map: map,
+//     });
+// }
 
 setTimeout(changeFont, 0700);
 
